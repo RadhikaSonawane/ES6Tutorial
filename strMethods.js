5dae6d2dfd9c915c28fab089adb6525aed70e1fd
RadhikaSonawane <41570546+RadhikaSonawane@users.noreply.github.com>
2020-06-21 16:53:03 +0200

    See some methods

---
+++ strMethods.js
@@ -0,0 +1,11 @@
+//ES6 strings methods
+
+let firstName ="Radhika";
+
+console.log(`${firstName}`.startsWith('R'));
+console.log(`${firstName}`.endsWith('a'));
+console.log(`${firstName}`.includes('Radhika'));
+console.log(`${firstName} `.repeat(5));
+console.log(`${firstName}`.replace('Radhika', 'Radha'));
+console.log(`${firstName}`.toLocaleUpperCase());
+console.log(`${firstName}`.toLocaleLowerCase());
