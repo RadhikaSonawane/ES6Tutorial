1167f829ae8bbcfeaef3e34cfa1f2523cb3f7bcd
RadhikaSonawane <41570546+RadhikaSonawane@users.noreply.github.com>
2020-06-21 16:11:39 +0200

    ES5 and ES6 template type

---
+++ templateString.js
@@ -0,0 +1,24 @@
+//ES5
+console.log("My Name is Radhika. " + "My last name is Sonawane");
+
+//ES6
+console.log(`My name is Radhika. My last name is Sonawane`);
+
+let name = "Radhika";
+let lastName = "Sonawane";
+
+//ES5
+console.log("my name is " + name + ". " + "My last name is " + lastName + ".");
+
+//ES6
+console.log(`My name is ${name}. My last name is ${lastName}.`);
+
+// we can add expression too
+let x = 20;
+let y = 50;
+
+//ES5
+console.log("Total is " + (x+y));
+
+//ES6
+console.log(`Total is ${x+y}`);
