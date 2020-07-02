4fedf1ad113be92bf2428ccf82c1b3c97b803ccc
RadhikaSonawane <41570546+RadhikaSonawane@users.noreply.github.com>
2020-07-02 14:59:22 +0200

    word scramble

--- scrambleGame/script.js
+++ scrambleGame/script.js
@@ -10,7 +10,22 @@ const createNewWords = () => {
     let ranNum = Math.floor(Math.random() * sWords.length);
     //console.log(ranNum);
     let neTempSwords = sWords[ranNum];
+    //console.log(newTempSwords.split(""))
     return neTempSwords;
+}
+
+const scrambleWord = (arr) =>{
+    for(let i = arr.length-1; i>0; i--){
+        let temp = arr[i];
+        console.log(temp)
+        let j = Math.floor(Math.random()*(i+1));
+        console.log(i);
+        console.log(j);
+
+        arr[i]= arr[j];
+        arr[j]= temp;
+    }
+    return arr;
 }
 
 btn.addEventListener('click', function(){
@@ -19,6 +34,23 @@ btn.addEventListener('click', function(){
         btn.innerHTML = "Guess";
         guess.classList.toggle('hidden');
         newWords=createNewWords();
-        scrambleWord();
+        randWords = scrambleWord(newWords.split(""));
+        console.log(randWords.join(""));
+        msg.innerHTML = `Guess the word ${randWords}`;
+        
+        } else{
+            let tempWord =  guess.value;
+            if(tempWord === newWords){
+                //console.log('correct');
+                play = false;
+                msg.innerHTML = `Awesome it's correct. it is ${newWords}`;
+                btn.innerHTML = "Start again";
+                guess.classList.toggle('hidden');
+                guess.value = "";
+            }else {
+                //console.log('inncorrect');
+                msg.innerHTML = `Sorry it's inncorrect. Please try again ${randWords}`;
+            
+            }
         }
 })
