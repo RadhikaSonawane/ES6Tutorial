286a13b15c4b27f1fc6a790e5bcc13e3de06d5cb
RadhikaSonawane <41570546+RadhikaSonawane@users.noreply.github.com>
2020-07-03 16:06:30 +0200

    math object

---
+++ mathObject.js
@@ -0,0 +1,18 @@
+/* 
+Math object in ES6
+Two methods
+1. sign()
+2. trunc()
+ */
+
+ // sign()
+ // if number is positive we will get console 1 if negative number console got -1
+ let number = 5.95;
+ console.log(Math.sign(number));
+
+ //trunc()
+ //this method help us to ger compalite number e.g. 5.55 means 5 or 5.99 means 6
+ console.log(Math.trunc(number));
+
+ //floor()
+ console.log(Math.floor(number));
